0f72cc9a45ee85adaa82bb71bffda6594f27f3cd
Sharma, Amit <Asharma@agtinternational.com>
2018-02-08 23:05:21 +0100

    Update video sample data.

--- source/server/sampleData/video.ts
+++ source/server/sampleData/video.ts
@@ -1,13 +1,99 @@
 const VIDEOS = [
   {
-    id: 'a',
-    name: 'Create a GraphQL Schema',
-    watched: true,
+    "id": "5ac7c9ee-fa53-489e-8ec5-31dbbbcf893c",
+    "name": "Pideo_5ac7c9ee-fa53-489e-8ec5-31dbbbcf893c",
+    "description": "This Pideo is has the id: 5ac7c9ee-fa53-489e-8ec5-31dbbbcf893c",
+    "watched": true,
+    "authorName": "Author_5ac7c9ee-fa53-489e-8ec5-31dbbbcf893c",
+    "url": "http://www.sampledata.com/5ac7c9ee-fa53-489e-8ec5-31dbbbcf893c"
   },
   {
-    id: 'b',
-    name: 'Ember.js CLI',
-    watched: false,
+    "id": "fe449f6b-c4f2-49f1-8eb8-7930511fab44",
+    "name": "Pideo_fe449f6b-c4f2-49f1-8eb8-7930511fab44",
+    "description": "This Pideo is has the id: fe449f6b-c4f2-49f1-8eb8-7930511fab44",
+    "watched": true,
+    "authorName": "Author_fe449f6b-c4f2-49f1-8eb8-7930511fab44",
+    "url": "http://www.sampledata.com/fe449f6b-c4f2-49f1-8eb8-7930511fab44"
+  },
+  {
+    "id": "6df1a08a-af78-450c-bd9b-fe85b6a44edc",
+    "name": "Pideo_6df1a08a-af78-450c-bd9b-fe85b6a44edc",
+    "description": "This Pideo is has the id: 6df1a08a-af78-450c-bd9b-fe85b6a44edc",
+    "watched": true,
+    "authorName": "Author_6df1a08a-af78-450c-bd9b-fe85b6a44edc",
+    "url": "http://www.sampledata.com/6df1a08a-af78-450c-bd9b-fe85b6a44edc"
+  },
+  {
+    "id": "9120be67-15d6-4202-9f43-7410e949edab",
+    "name": "Pideo_9120be67-15d6-4202-9f43-7410e949edab",
+    "description": "This Pideo is has the id: 9120be67-15d6-4202-9f43-7410e949edab",
+    "watched": true,
+    "authorName": "Author_9120be67-15d6-4202-9f43-7410e949edab",
+    "url": "http://www.sampledata.com/9120be67-15d6-4202-9f43-7410e949edab"
+  },
+  {
+    "id": "2b64018e-a62b-4a7a-8122-e5e1c9e983fa",
+    "name": "Pideo_2b64018e-a62b-4a7a-8122-e5e1c9e983fa",
+    "description": "This Pideo is has the id: 2b64018e-a62b-4a7a-8122-e5e1c9e983fa",
+    "watched": true,
+    "authorName": "Author_2b64018e-a62b-4a7a-8122-e5e1c9e983fa",
+    "url": "http://www.sampledata.com/2b64018e-a62b-4a7a-8122-e5e1c9e983fa"
+  },
+  {
+    "id": "6d351618-ebda-4e61-90ec-c35aaab10c13",
+    "name": "Pideo_6d351618-ebda-4e61-90ec-c35aaab10c13",
+    "description": "This Pideo is has the id: 6d351618-ebda-4e61-90ec-c35aaab10c13",
+    "watched": true,
+    "authorName": "Author_6d351618-ebda-4e61-90ec-c35aaab10c13",
+    "url": "http://www.sampledata.com/6d351618-ebda-4e61-90ec-c35aaab10c13"
+  },
+  {
+    "id": "885b89dd-4e99-4784-854f-bfb4d7657b15",
+    "name": "Pideo_885b89dd-4e99-4784-854f-bfb4d7657b15",
+    "description": "This Pideo is has the id: 885b89dd-4e99-4784-854f-bfb4d7657b15",
+    "watched": true,
+    "authorName": "Author_885b89dd-4e99-4784-854f-bfb4d7657b15",
+    "url": "http://www.sampledata.com/885b89dd-4e99-4784-854f-bfb4d7657b15"
+  },
+  {
+    "id": "632d867d-dd05-4158-85c2-ab080e1a4bf6",
+    "name": "Pideo_632d867d-dd05-4158-85c2-ab080e1a4bf6",
+    "description": "This Pideo is has the id: 632d867d-dd05-4158-85c2-ab080e1a4bf6",
+    "watched": true,
+    "authorName": "Author_632d867d-dd05-4158-85c2-ab080e1a4bf6",
+    "url": "http://www.sampledata.com/632d867d-dd05-4158-85c2-ab080e1a4bf6"
+  },
+  {
+    "id": "ab3d1f73-6749-4756-b9d9-98bd54eae461",
+    "name": "Pideo_ab3d1f73-6749-4756-b9d9-98bd54eae461",
+    "description": "This Pideo is has the id: ab3d1f73-6749-4756-b9d9-98bd54eae461",
+    "watched": true,
+    "authorName": "Author_ab3d1f73-6749-4756-b9d9-98bd54eae461",
+    "url": "http://www.sampledata.com/ab3d1f73-6749-4756-b9d9-98bd54eae461"
+  },
+  {
+    "id": "7c3e1398-d9a3-4f8a-b602-f86777e3714e",
+    "name": "Pideo_7c3e1398-d9a3-4f8a-b602-f86777e3714e",
+    "description": "This Pideo is has the id: 7c3e1398-d9a3-4f8a-b602-f86777e3714e",
+    "watched": true,
+    "authorName": "Author_7c3e1398-d9a3-4f8a-b602-f86777e3714e",
+    "url": "http://www.sampledata.com/7c3e1398-d9a3-4f8a-b602-f86777e3714e"
+  },
+  {
+    "id": "743eb98f-7dcd-4cc2-a166-611bcca4efe2",
+    "name": "Pideo_743eb98f-7dcd-4cc2-a166-611bcca4efe2",
+    "description": "This Pideo is has the id: 743eb98f-7dcd-4cc2-a166-611bcca4efe2",
+    "watched": true,
+    "authorName": "Author_743eb98f-7dcd-4cc2-a166-611bcca4efe2",
+    "url": "http://www.sampledata.com/743eb98f-7dcd-4cc2-a166-611bcca4efe2"
+  },
+  {
+    "id": "851fdeba-51d7-4366-a69b-120bdfbcdb68",
+    "name": "Pideo_851fdeba-51d7-4366-a69b-120bdfbcdb68",
+    "description": "This Pideo is has the id: 851fdeba-51d7-4366-a69b-120bdfbcdb68",
+    "watched": true,
+    "authorName": "Author_851fdeba-51d7-4366-a69b-120bdfbcdb68",
+    "url": "http://www.sampledata.com/851fdeba-51d7-4366-a69b-120bdfbcdb68"
   },
 ]
 
